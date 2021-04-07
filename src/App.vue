90262597479f48326e5091f0c6ffa0054e4633c1
Okanclskn <okan35342@gmail.com>
2021-04-07 14:08:04 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="container">
+    <h3>Başvuru Formu</h3>
+    <hr>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="panel panel-warning">
+          <div class="panel-heading">
+            <h4>Başvuru Formu</h4>
+          </div>
+          <div class="panel-body">
+            <form>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <label for="email">Kullanıcı Adı</label>
+                    <input type="text" id="username" class="form-control" v-model="userData.userName">
+                  </div>
+                  <div class="form-group">
+                    <label for="password">Şifre</label>
+                    <input type="password" id="password" class="form-control" v-model="userData.password">
+                  </div>
+                  <div class="form-group">
+                    <label for="age">Yaş</label>
+                    <input type="number" id="age" class="form-control" v-model="userData.age">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <label for="message">Açıklama</label><br>
+                  <textarea id="message" rows="3" class="form-control" v-model="userData.description"></textarea>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <label>
+                      <input v-model="userData.interests" type="checkbox" value="yazilim"> Yazılım
+                    </label>
+                    <label>
+                      <input v-model="userData.interests" type="checkbox" value="donanim"> Donanım
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <label>
+                    <input v-model="userData.gender" type="radio" value="erkek"> Erkek
+                  </label>
+                  <label>
+                    <input v-model="userData.gender" type="radio" value="kadın"> Kadın
+                  </label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 from-group">
+                  <label>Şehir</label>
+                  <select v-model="userData.selectedCity" class="form-control">
+                    <option v-for="(city,index) in userData.cities" :key="index">{{city}}</option>
+                  </select>
+                </div>
+              </div>
+              <hr>
+              <div class="row">
+                <div class="col-md-12">
+                  <button @click.prevent="submit" class="btn btn-primary">Gönder!
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6" v-if="isSubmit">
+        <div class="panel panel-info">
+          <div class="panel-heading">
+            <h4>Form Verileri</h4>
+          </div>
+          <div class="panel-body">
+            <p>Kullanıcı Adı: {{userData.userName}}</p>
+            <p>Şifre: {{userData.password}}</p>
+            <p>Yaş: {{userData.age}}</p>
+            <p style="white-space: pre;">Açıklama: {{userData.description}} </p>
+            <p><strong>İlgi Alanları</strong></p>
+            <ul>
+              <li v-for="(interest, index) in userData.interests" :key="index">{{interest}}</li>
+            </ul>
+            <p>Cinsiyet: {{userData.gender}}</p>
+            <p>Şehir: {{userData.selectedCity}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        userData: {
+          userName: "",
+          password: "",
+          age: null,
+          description: "",
+          interests: [],
+          gender: "",
+          cities: ["Adana", "Adıyaman", "Afyon", "Ağrı", "Amasya", "Ankara", "Antalya", "Artvin", "Aydın", "Balıkesir", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Isparta", "İçel (Mersin)", "İstanbul", "İzmir", "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Uşak", "Van", "Yozgat", "Zonguldak", "Aksaray", "Bayburt", "Karaman", "Kırıkkale", "Batman", "Şırnak", "Bartın", "Ardahan", "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Düzce"],
+          selectedCity: "",
+          
+        },
+        isSubmit: false
+      }
+    },
+    methods: {
+      submit(){
+          this.isSubmit = true
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
